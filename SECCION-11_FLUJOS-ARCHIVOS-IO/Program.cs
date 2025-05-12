3e1cfe658d3c6abbed3ff8cb539683368276b021
DaGcc <gutierrezcallazacadaniel2020@gmail.com>
2025-05-11 21:53:35 -0500

    Modificacion de seccion-08

--- SECCION-11_FLUJOS-ARCHIVOS-IO/Program.cs
+++ SECCION-11_FLUJOS-ARCHIVOS-IO/Program.cs
@@ -7,16 +7,16 @@ class Program
     public static void Main()
     {
 
-        byte[] bufferWithData  = Encoding.UTF8.GetBytes("Hola Mundo");// buffer con un tamaño definido 
+        byte[] bufferWithData = Encoding.UTF8.GetBytes("Hola Mundo");// buffer con un tamaño definido 
 
-        using (MemoryStream mst = new MemoryStream())
+        using (MemoryStream mst = new MemoryStream())// stream con un tamaño dinamico 
         {
-            Console.WriteLine("MemoryStream Antes:");  
+            Console.WriteLine("MemoryStream Antes:");
             Console.WriteLine($"Length   :  {mst.Length}");
             Console.WriteLine($"Position :  {mst.Position}");
             Console.WriteLine($"Capacity :  {mst.Capacity}");
 
-            mst.Write(bufferWithData, 0, bufferWithData.Length);
+            mst.Write(bufferWithData, 0, bufferWithData.Length);// Aqui el stream lee los datos de buffer y los escribe en su buffer dinamico 
 
             Console.WriteLine("MemoryStream Despues:");
             Console.WriteLine($"Length   :  {mst.Length}");
@@ -28,14 +28,14 @@ class Program
 
         byte[] buffer = new byte[1024]; //es un buffer que representa un espacio en memoria de 1024 KB
         byte[] bufferData = Encoding.UTF8.GetBytes("Datos que seran escritos a buffer");
-        using (MemoryStream mst = new MemoryStream(bufferData))
+        using (MemoryStream mst = new MemoryStream(bufferData))//el stream ya tiene un tamaño fijo debido al buffer pasado como parametro, 
         {
             Console.WriteLine("MemoryStream Antes:");
             Console.WriteLine($"Length   :  {mst.Length}");
             Console.WriteLine($"Position :  {mst.Position}");
             Console.WriteLine($"Capacity :  {mst.Capacity}");
 
-            mst.Read(buffer, 0, bufferData.Length);   
+            mst.Read(buffer, 0, bufferData.Length);
 
             Console.WriteLine("MemoryStream Despues:");
             Console.WriteLine($"Length   :  {mst.Length}");
@@ -44,6 +44,15 @@ class Program
         }
         string result = Encoding.UTF8.GetString(buffer);
         Console.WriteLine(result);
+
+
+
+        //CREACION DE DIRECTORIO 
+        string route = Directory.GetCurrentDirectory();
+
+
+        Console.WriteLine(route);
+
 
     }
 
